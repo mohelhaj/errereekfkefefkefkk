427c4a3f68aae048897bb43788996c7db2af9ead
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-26 05:43:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,31 +36,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 
 
 
-const Discord = require('discord.js');
 
-const Util = require('discord.js');
-
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const fs = require('fs');
-
-const gif = require("gif-search");
-
-const client = new Discord.Client({disableEveryone: true});
-
-const prefix = "!";
-/////////////////////////
-////////////////////////
 
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
