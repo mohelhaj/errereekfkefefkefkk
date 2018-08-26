ecf7d4573fcada791152e4e652b31cd088bae57f
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-27 01:15:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,4 +93,18 @@ client.on("message", message => { //clear
        
   });
 
+         client.on('message', message => {
+            if (message.content.startsWith(prefix + "bot")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' السيرفرات🌐',`[${client.guilds.size}]  `)
+.addField(' الاعضاء👥 ',` [${client.users.size}] `)
+.addField('الرومات📚 ',`[${client.channels.size}]`) 
+.addField(' البنق🚀 ',`[${Date.now() - message.createdTimestamp}]`) 
+.addField('مصمم  + صاحب البوت ',`Mr.LOVE`)
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
