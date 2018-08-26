0d3f58cbdb67dd4a744517c2c45ec680a8fefd52
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-26 05:47:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,28 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '.'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
+const Util = require('discord.js');
 
+const getYoutubeID = require('get-youtube-id');
 
+const fetchVideoInfo = require('youtube-info');
 
+const YouTube = require('simple-youtube-api');
 
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 
+const queue = new Map();
 
+const ytdl = require('ytdl-core');
 
+const fs = require('fs');
 
+const gif = require("gif-search");
 
+const client = new Discord.Client({disableEveryone: true});
 
+const prefix = "!";
+/////////////////////////
+////////////////////////
 
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
@@ -355,14 +341,5 @@ client.on('message', message => {
       message.channel.send(helpEmbed);
     }
 });
-
-
-
-
-
-
-
-
-
 
 client.login(process.env.BOT_TOKEN);
