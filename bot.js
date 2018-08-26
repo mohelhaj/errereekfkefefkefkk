aa4b672fe09867d0775fb64da118fbb9a314d9d1
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-27 01:26:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,4 +95,21 @@ var prefix = "!";
  }
 });
 
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "-roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
