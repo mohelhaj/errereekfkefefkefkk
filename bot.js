65c38a85b4712ae7a6e88e974fc0075ca33c8040
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-26 06:21:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,10 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+client.on('message' , message => {
+if(message.content === 'عدد') {
+    message.channel.send(`**عدد الاشخاص الموجودين بـ  الرومات الصوتيه : ${message.guild.members.filter(g => g.voiceChannel).size}**`);
+}
+});
 
 client.login(process.env.BOT_TOKEN);
