b87892aecbe9242288574b931bd39eeed3443d5a
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-27 21:55:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -329,22 +329,6 @@ client.on("message", msg => {
   }
 });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-}
-
 client.on('message', message => { 
     var p = "*"
         if (message.content.startsWith(p + `alpha`)) {
