6741ab0f1bbfe074ee15079c5d8ac0f97d7996b0
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-28 12:37:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -574,16 +574,6 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if(message.content.includes('discord.gg')){
-                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? 🤔   **');
-        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-        message.delete()
-    return message.reply(`** No Invite Links :angry: !**`)
-    }
-}
-});
-
-client.on('message', message => {
 var prefix = "*";
 
     if (message.author.id === client.user.id) return;
