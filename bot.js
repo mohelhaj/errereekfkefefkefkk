9007c6bcb121e995a23534aec5213d04ed02b54d
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-28 17:27:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -608,5 +608,21 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 }
 });
 
+client.on('message', ra3d => {
+var prefix = "$";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'cc')) {
+    if(!args) return ra3d.channel.send('يرجا عليك كتابه رقم الالوان');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('**انت لا تمتلك صلاحيه MANAGE_ROLES**'); 
+              ra3d.channel.send(:white_check_mark: |تم صنع ${args} الوان);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x <${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+
 client.login(process.env.BOT_TOKEN);
 
