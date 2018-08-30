95fdc0fe9c00cca0638b9632ee12ce035f8ee9e5
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-30 14:37:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,46 +220,63 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 }
 });
 
-client.on('message' , async (message) => {
-var prefix = "-"
-    if(message.content.startsWith(prefix + "inv")) {
-if(message.author.bot) return;
-if(!message.channel.guild) return message.reply(' Error : ` Guild Command `');
-  var invites = await message.guild.fetchInvites();
-    invites = invites.array();
-    arraySort(invites, 'uses', { reverse: true });
-    let possibleInvites = ['User Invited |  Uses '];
-    invites.forEach(i => {
-        if (i.uses === 0) { 
-            return;
-        }
-      possibleInvites.push(['\n\ ' +'<@'+ i.inviter.id +'>' + '  :  ' +   i.uses]);
-      if (i.uses === 10) {//يمديك تعدل رقم وصول العدد حق الانفايت الى اأقل أو أكثر
-          message.member.addRole(message.member.guild.roles.find("Member",""))//هنآ أسم ألرتبه اللي تجيهه
-.catch(RebeL =>{
-console.log('Error: ' + RebeL);
-});
-}
-if (i.uses === 20) {
-message.member.addRole(message.member.guild.roles.find("name",""))
-.catch(RebeL =>{
-console.log('Error: ' + RebeL);
-});
-}
-if (i.uses === 30) {
-message.member.addRole(message.member.guild.roles.find("name",""))
-.catch(RebeL =>{
-console.log('Error: ' + RebeL);
-});
-      }//معلومه بسيطه يمديك تكرر العمليهه أكثر من مره
-    })
-    const embed = new Discord.RichEmbed()
- .setColor('#36393e')
-    .addField("Top Invites." ,${(possibleInvites)})
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "tC")) {
+    await message.channel.send("ارسل اسم الروم").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let name = '';
+    let time = '';
+    let type = '';
+    let limit = '';
 
-    message.channel.send(embed)
-    }
-});
+
+    message.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
+    .then(collected => {
+      name = collected.first().content
+      collected.first().delete()
+
+
+
+e.edit("ارسل مدة الروم بالدقائق لااقل من 2 ولا اعلى من 180")
+message.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
+.then(co => {
+if(isNaN(co.first().content)) return message.reply("الوقت بالدقائق ! ارقام فقطٍ");
+if(co.first().content > 180 || co.first().content < 2) return message.channel.send("لا اقل من دقيقتان ولا اكثر من 180 دقيقه")
+  time = co.first().content
+co.first().delete()
+  e.edit("ارسل نوع الروم text, voice")
+message.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
+.then(col => {
+  type = col.first().content
+col.first().delete()
+e.edit("ارسل عدد الاعضاء الذين يستطيعون الدخول")
+message.channel.awaitMessages(filter, { max: 1, time: 20000, errors: ['time'] })
+.then(coll => {
+  if(isNaN(coll.first().content)) return message.reply("عدد الاعضاء يكون بالارقام فقط");
+    limit = coll.first().content
+coll.first().delete()
+
+  e.edit("جاري اعداد الغرفه الرجاء الانتضار...")
+  message.guild.createChannel(name, type).then(c => {
+    c.edit({
+      userLimit: limit
+    })
+    setTimeout(() => {
+      c.delete()
+      message.channel.send("تم انقضاء الوقت")
+    }, Math.floor(time*60000))
+
+  })
+  e.edit("تم انشاء الغرفه استمتع")
+
+})
+})
+})
+})
+})
+
+  }
+})
 
 
 client.login(process.env.BOT_TOKEN);
