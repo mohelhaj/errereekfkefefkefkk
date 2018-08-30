9c8925d8761ea6584efb9fde872da227d9c3e824
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-30 14:08:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,5 +220,24 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 }
 });
 
+client.on('message', message => {
+    if (message.content.startsWith("-رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 2,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("تم ارسال الرابط برسالة خاصة")
+
+message.author.send(**مدة الرابط : يـوم
+عدد استخدامات الرابط : 2**)
+
+
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
 
